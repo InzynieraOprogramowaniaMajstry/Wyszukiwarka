04aa4b243684e5c92e883cf38a48939de7351d56
Adrian Jaśkowiec <ajaskowiec@onet.eu>
2023-01-16 13:07:38 +0100

    App broke, fixes xd

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import sys
 import os
-from initialize_objects import app
-from controllers_and_routes.all_routes import user_bp
 SCRIPT_DIR = os.path.dirname(__file__)
 sys.path.append(SCRIPT_DIR)
+from initialize_objects import app
+from controllers_and_routes.all_routes import user_bp
 
 app.register_blueprint(user_bp, url_prefix='/')
 
--- initialize_objects.py
+++ initialize_objects.py
@@ -1,11 +1,12 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from models.wolne_lektury_api import WolneLekturyAPI
-from config import Conf
 import sys
 import os
 SCRIPT_DIR = os.path.dirname(__file__)
 sys.path.append(SCRIPT_DIR)
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from models.wolne_lektury_api import WolneLekturyAPI
+from config import Conf
 
 app = Flask(__name__)
 app.config.from_object(Conf)
